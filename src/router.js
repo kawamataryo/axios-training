83ff77d3ee261122557bb48cfb49eff1c47b243e
ryo <ba068082@gmail.com>
2018-12-01 23:03:21 +0900

    fist commit

--- src/router.js
+++ src/router.js
@@ -1,26 +1,40 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from "vue"
+import Router from "vue-router"
+import Home from "./views/Home"
+import About from "./views/About"
+import User from "./views/User"
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
       name: "home",
-      component: Home
+      component: Home,
+      meta: {
+        eopPage: "hoge"
+      }
     },
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: About,
+      meta: {
+        eopPage: "About"
+      }
+    },
+    {
+      path: "/user",
+      name: "user",
+      component: User,
+      meta: {
+        eopPage: "User"
+      }
     }
   ]
-});
+})
+
+export default router
